e42070135733efe737ad42f15183a62547fab1ab
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2022-03-27 14:37:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ from discord.ext import commands
 import asyncio
 from data import data
 
+#'Token' is Discord bot's ID.
 my_secret = os.environ['Token']
 intents = discord.Intents.default()
 intents.members = True
+
+#This lets the bot go searching if there's exclamation mark.
 client = commands.Bot(command_prefix = "!", intents = intents)
 
+#Whenever the script is active, the bot is also online.
 @client.event
 async def on_ready():
   print('{0.user}'.format(client)+' is online')
 
+#If the user types "!odusVerify", it will send a private message.
 @client.command()
 async def odusVerify(ctx):
 
@@ -23,7 +28,8 @@ async def odusVerify(ctx):
   
   def check(m):
     return m.author.id == ctx.author.id
-    
+  
+#After the message from the bot is sent, it waits for the user input. If 60 seconds passes, it will timeout.
   try:
     
     msg1 = await client.wait_for('message', check=check, timeout=60)
@@ -33,6 +39,7 @@ async def odusVerify(ctx):
   except asyncio.TimeoutError:
     await ctx.author.send("`Timeout, Please try again by using (!odusVerify)`")  
 
+#If the user enters the ID, it will send another message and wait for another input.
   try:
     if ID:
       await ctx.author.send("`Enter your Password (Same as OdusPlus):`") 
@@ -45,6 +52,7 @@ async def odusVerify(ctx):
   except asyncio.TimeoutError:
     await ctx.author.send("`Timeout. Please try again by using (!odusVerify)`")
 
+#Once there's ID and PASS input it will run dataResult method once and the variable placeHold will hold either CS/IT/IS    
   def dataResult():
     return data(ID,PASS)  
 
@@ -77,6 +85,6 @@ async def odusVerify(ctx):
     await member.add_roles(role)
     await user.send("`IS role has been assigned to you`")
     
-    
+#This pings the script so it keeps running all the time.
 keep_alive()
 client.run(my_secret)
